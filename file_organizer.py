ea47f357db936382221e6afdc997d1ad92594d9f
ggwellplayed4101 <bhargavchataut101@gmail.com>
2024-11-25 21:32:32 -0600

    Final Version

--- file_organizer.py
+++ file_organizer.py
@@ -1,8 +1,15 @@
 import os, shutil
 
 path= input("Enter path: ")
-files=os.listdir(path)
-"""print (files)"""
+files=os.listdir(path)   #listing files in the given path
 
 for file in files:
-    filename,extension=os.path.splitext(file)
+    filename,extension=os.path.splitext(file)    #seperating path and extension in 
+    extension=extension[1:]
+
+    if os.path.exists(path+"/"+extension):     #checks if there is a folder for a type of extension
+        shutil.move(path+"/"+file, path+"/"+extension+"/"+file)    #changes directory of the file
+    
+    else:
+        os.makedirs(path+"/"+extension)        #makes an folder for the type of extension
+        shutil.move(path+"/"+file, path+"/"+extension+"/"+file)     #changes directory of the file
